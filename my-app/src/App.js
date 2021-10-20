6292520415e91a75f47a4c235486ee918785d69c
hunterZh37 <zzhang978@wisc.edu>
2021-10-20 18:42:49 -0500

    fixed the bug

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import frontPage from "./front-page.jpg"
 
 function App() {
   return (
-    ""
+    <>
+      <img src = {frontPage}/>
+    </>
   );
 }
 
