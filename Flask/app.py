5825f15d5806af69571e171dcdeb71dc3b3c4206
Anthony Lam <alam9@wisc.com>
2021-10-20 19:03:40 -0500

    Added db models for occassion and filters

--- Flask/app.py
+++ Flask/app.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+
+class Occassion(db.Model):
+    name = db.Column(db.String(100))
+    filter = db.Column(db.String(100))
+
+class Filters(db.Model):
+    name = db.Column(db.String(100))
+    tag = db.Column(db.String(100))
 
 
 @app.route('/')
