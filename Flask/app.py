b83732b789410d0717634d30e95cacdd5e74fe87
unknown <fardeenfmeeran@gmail.com>
2021-10-20 20:58:30 -0500

    Added local SQL script file

--- Flask/app.py
+++ Flask/app.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from mySqlDB import createOccasions
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    occasionlist = createOccasions()
+    return 'Hello, World! the first occasion is ' + str(occasionlist[0][1])
+if __name__ == "__main__":
+    app.run()
