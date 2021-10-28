58af9f27dca684012e23702bc36c548e175438e4
Anthony Lam <alam9@wisc.com>
2021-10-27 23:27:14 -0500

    removed sqlalchemy code

--- Flask/app.py
+++ Flask/app.py
@@ -1,37 +1,10 @@
 from flask import Flask
 import mySqlDB
-from flask_sqlalchemy import SQLAlchemy
 import json
 app = Flask(__name__)
 
 mySqlDB.connect()
 mySqlDB.createTables()
-# STRING_LENGTH = 100
-# PHONE_NUMBER_LENGTH = 15
-# class Occassion(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(STRING_LENGTH))
-#     filter = db.Column(db.String(STRING_LENGTH))
-
-# class Filters(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(STRING_LENGTH))
-#     tag = db.Column(db.String(STRING_LENGTH))
-
-# class Buisness_tags(db.Model):
-#     tag_id = db.Column(db.Integer, primary_key=True)
-#     id = db.Column(db.Integer)
-
-# class Buisness_filters(db.Model):
-#     filter_id = db.Column(db.Integer, primary_key=True)
-#     id = db.Column(db.Integer)
-#     filter = db.Column(db.String(STRING_LENGTH))
-
-# class Buisness(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.Text)
-#     phone_number = db.Column(db.String(20))
-#     address = db.Column(db.Text)
     
 
 @app.route('/occasions', methods=['GET'])
