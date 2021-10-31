e0189c2dcc92675c0971dfc413de22ec4cf15e52
Anthony Lam <alam9@wisc.com>
2021-10-31 14:33:57 -0500

    fixed filter tag query

--- Flask/mySqlDB.py
+++ Flask/mySqlDB.py
@@ -38,14 +38,17 @@ def createOccasions():
   connection.commit()
 
 def createOccasionsFilters():
-  filters = ['Food', 'shopping', 'sightseeing']
+  # filters = ['Food', 'shopping', 'sightseeing']
   cursor.execute(f"CREATE table if not exists occasionsfilters (id int({ID_SIZE}), occasionid int({ID_SIZE}), filter varchar({STRING_LENGTH}), PRIMARY KEY(id))")
-  for i in range(3):
-    cursor.execute(f"REPLACE INTO occasionsfilters (id, occasionid, filter) VALUES ({i},0,\"{filters[i]}\")")
+  # for i in range(3):
+  #   cursor.execute(f"REPLACE INTO occasionsfilters (id, occasionid, filter) VALUES ({i},0,\"{filters[i]}\")")
   connection.commit()
 
 def createFiltersTags():
+  # tags = ['Mexican','Japanese','Chinese']
   cursor.execute(f"CREATE table if not exists filterTags (filterid int({ID_SIZE}), tag varchar({STRING_LENGTH}), PRIMARY KEY(filterid,tag))")
+  # for i in range(3):
+  #   cursor.execute(f"REPLACE INTO filterTags (filterid,tag) VALUES (0,'{tags[i]}')")
   connection.commit()
 
 def createBuisness():
@@ -62,10 +65,19 @@ def getOccasions():
   return results
 
 def getFilters(occasion):
-  query = f"SELECT f.filter from occasionsfilters f, occasions o where o.id=f.occasionid and o.name=\"{occasion}\""
+  output = {}
+  query = f"SELECT f.id, f.filter from occasionsfilters f, occasions o where o.id=f.occasionid and o.name=\"{occasion}\""
   cursor.execute(query)
-  results= cursor.fetchall()
-  return results
+  filters= cursor.fetchall()
+  for filter in filters:
+    id,name = filter
+    output[name] = getTags(id)
+  return output
+
+def getTags(filter):
+  cursor.execute(f"SELECT tag from filterTags where filterid='{filter}'")
+  results=cursor.fetchall()
+  return [result[0] for result in results]
 
 def closeConnection():
   connection.close()
