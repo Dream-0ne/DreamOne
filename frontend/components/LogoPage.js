8118367ed193e4a6e7e978a9402f3b3c885e4cb6
unknown <fardeenfmeeran@gmail.com>
2021-11-04 23:12:34 -0500

    Made timeout function to move from logo to occasions

--- frontend/components/LogoPage.js
+++ frontend/components/LogoPage.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import { ImageBackground, StyleSheet, Button, View } from "react-native";
 import image from "../assets/frontPageLogo.png";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Component } from "react/cjs/react.development";
+
 
 
 
 export default function LogoPage ({navigation}) {
+  useEffect(() => {
+    setTimeout(function(){
+    navigation.navigate('Occasions');
+  }, 2000); //run this after 3 seconds
+});
     return (
         <View style={styles.container}>
           <ImageBackground source={image} resizeMode="cover" style={styles.image}>
           </ImageBackground>
-          <Button title  = "presss meeee!!??!" onPress = {() => {navigation.navigate('Occasions')}}/>
+          {/* <Button title  = "presss meeee!!??!" onPress = {() => {navigation.navigate('Occasions')}}/> */}
           
         </View>
       );
